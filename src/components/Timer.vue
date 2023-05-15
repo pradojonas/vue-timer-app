e3f705344ca5d33dc3b10508c5a272c6efe2c2c5
Jonas Prado <pradojonas64@gmail.com>
2023-05-15 19:23:47 -0300

    Creating components

---
+++ src/components/Timer.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>{{ header + number }}</h1>
+
+<li v-for="item in items" :key="item.id">{{item.label}}</li>
+</template>
+
+<script>
+export default {
+    props: ['number'],
+    data () {
+        return {
+            header: 'Shopping List App',
+            items: [
+                {id: 1, label:'10 party hats'},
+                {id: 2, label:'2 board games'},
+                {id: 3, label:'20 cups'}]
+        }
+    },  
+    methods: {
+    },
+    created () {
+    }
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
-    <div class="about">
-        <h1>About</h1>
-    </div>
+    <Timer number="1"></Timer>
+    <Timer number="2"></Timer>
 </template>
+ 
+<script>
+import Timer from '@/components/Timer.vue'
+    export default {
+        components: {
+            Timer
+        },
+        data () {  
+        },  
+        methods: {
+            
+        },
+        created () {
+            
+        }
+    }
+</script>
