6ce6a56fe5d531c60bd4354db63f5ee6e48cc5a9
Jonas Prado <pradojonas64@gmail.com>
2023-05-15 20:27:36 -0300

    Adding countdown

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,24 +1,50 @@
 <template>
-    <h1>{{ header + number }}</h1>
-
-<li v-for="item in items" :key="item.id">{{item.label}}</li>
+    <h1>{{ timerText }}</h1>
+    <h2>{{ remainingSeconds }}</h2 >
 </template>
 
 <script>
 export default {
-    props: ['number'],
+    emits: ['countdown'],
+    props: {
+        timer: Object   
+    },
+    computed: {
+        timerText() {
+            return this.timer.name + ': ' + this.c;
+        }
+    },
     data () {
         return {
-            header: 'Shopping List App',
-            items: [
-                {id: 1, label:'10 party hats'},
-                {id: 2, label:'2 board games'},
-                {id: 3, label:'20 cups'}]
-        }
-    },  
-    methods: {
+            remainingSeconds: 0,
+            counting: false,
+            header: 'Timer:'
+        };
     },
-    created () {
+    watch: {
+        remainingSeconds: {
+            handler(value) {
+                if (value > 0) {
+                    setTimeout(() => {
+                        this.remainingSeconds--;
+                    }, 1000);
+                }
+            },
+            immediate: true // This ensures the watcher is triggered upon creation
+        }
+    },
+    mounted() {
+        this.remainingSeconds = this.timer.initialSeconds;
+    },
+    methods: {
+        startCountdown() {
+            this.counting = true;
+            this.remainingSeconds = this.timer.initialSeconds;
+        },
+        onCountdownEnd() {
+            this.counting = false;
+            this.$emit('countdown', this.timer.name);
+        },
     }
 }
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,21 +1,30 @@
 <template>
-    <Timer number="1"></Timer>
-    <Timer number="2"></Timer>
+
+<div>
+    <button @click="startCountdown">Iniciar</button>
+</div>    
+<Timer :timer=timers[0] @countdown="countdown"></Timer>
+<Timer :timer=timers[1] @countdown="countdown"></Timer>
 </template>
  
 <script>
 import Timer from '@/components/Timer.vue'
-    export default {
-        components: {
-            Timer
-        },
-        data () {  
-        },  
-        methods: {
-            
-        },
-        created () {
-            
+export default {
+    components: {
+        Timer
+    },
+    data () {
+        return {
+            timers: [
+                {name: 'Boss', initialSeconds: 10 },
+                {name: 'Kish', initialSeconds: 15 },
+            ]
+        };
+    },
+    methods: {
+        countdown(seconds) {
+            console.log("olá "+seconds);
         }
     }
+}
 </script>
