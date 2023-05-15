3f9491ffe5475db5d19a532fa4f8c130b9d36403
Jonas Prado <pradojonas64@gmail.com>
2023-05-15 18:56:25 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<template>
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>
+
+  <router-view></router-view>
+</template>
+
+<style scoped>
+</style>
